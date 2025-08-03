931be757ce83bc768fce45bbf70bacfd7b42fc6e
coder-Rishi05 <rawatrishi1121@gmail.com>
2025-08-03 09:18:44 +0530

    corrected  api fetching using axios

--- Frontend/src/Pages/HomePage.jsx
+++ Frontend/src/Pages/HomePage.jsx
@@ -1,19 +1,19 @@
 import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import RateLimit from "../components/RateLimit";
+import axios from "axios";
 
 const HomePage = () => {
   const [rate_limit, setRate_limit] = useState(true);
   const [notes, setNotes] = useState([]);
   const [loading, setLoading] = useState(true);
 
-
   useEffect(() => {
     const fetchNotes = async () => {
       try {
-        const res = await fetch("http://localhost:5173/api/notes");
-        const data = await res.json();
-        console.log(data);
+        const res = await axios.post("http://localhost:5173/api/notes");
+
+        console.log(res.data);
       } catch (err) {
         console.log("Problem in getting the request ", err);
       }
