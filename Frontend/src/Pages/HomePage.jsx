dbdbbb015783ac738de0608a14483e48af7bc010
coder-Rishi05 <rawatrishi1121@gmail.com>
2025-08-04 17:12:44 +0530

    fixed ratelimiter ui error

--- Frontend/src/Pages/HomePage.jsx
+++ Frontend/src/Pages/HomePage.jsx
@@ -2,9 +2,10 @@ import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import RateLimit from "../components/RateLimit";
 import axios from "axios";
+import toast from "react-hot-toast";
 
 const HomePage = () => {
-  const [rate_limit, setRate_limit] = useState(true);
+  const [rate_limit, setRate_limit] = useState(false);
   const [notes, setNotes] = useState([]);
   const [loading, setLoading] = useState(true);
 
@@ -13,12 +14,20 @@ const HomePage = () => {
       try {
         const res = await axios.get("http://localhost:1234/api/notes");
         const data = res.data;
-        console.log(data )
+        setNotes(data);
+        console.log(data);
+        setRate_limit(false);
       } catch (err) {
         console.log("Problem in getting the request ", err);
+
+        if (err.response?.status === 429) setRate_limit(true);
+        else toast.error("Failed to load notes");
+        setRate_limit(true);
+      } finally {
+        setLoading(false);
       }
     };
-    // fetchNotes(); 
+    fetchNotes();
   }, []);
 
   return (
@@ -26,6 +35,9 @@ const HomePage = () => {
       <Navbar />
 
       {rate_limit && <RateLimit />}
+      <div className="max-w-7xl mx-auto p-4 mt-6 ">
+      
+      </div>
     </div>
   );
 };
