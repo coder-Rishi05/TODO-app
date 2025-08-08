825413d6c7dcb944717ff763180c9812092fdd21
coder-Rishi05 <rawatrishi1121@gmail.com>
2025-08-08 23:46:47 +0530

    added delete note functionality

--- Frontend/src/Pages/HomePage.jsx
+++ Frontend/src/Pages/HomePage.jsx
@@ -5,6 +5,8 @@ import axios from "axios";
 import toast from "react-hot-toast";
 import NoteCard from "../components/NoteCard";
 import api from "../lb/axios";
+import { Loader } from "lucide-react";
+import NotesNotFound from "../components/NotesNotFound";
 
 const HomePage = () => {
   const [rate_limit, setRate_limit] = useState(false);
@@ -34,21 +36,21 @@ const HomePage = () => {
 
   return (
     <div className="min-h-screen">
-      
       <Navbar />
       {rate_limit && <RateLimit />}
       <div className="max-w-7xl mx-auto p-4 mt-6 ">
         {loading && (
-          <div className="text-center text-primary py-10">
-            {" "}
-            Loading notes...{" "}
+          <div className="text-center flex items-center justify-center text-primary py-10">
+            <Loader className="size-10 rotate-360  transition-all duration-300 " />
           </div>
         )}
+
+        {notes.length === 0 && !rate_limit && <NotesNotFound />}
 
         {notes.length > 0 && !rate_limit && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 ">
             {notes.map((note, index) => (
-              <NoteCard data={note} key={note._id} />
+              <NoteCard data={note} key={note._id} setNotes={setNotes} />
               // <div key={note._id} >
 
               //   {note.title} |
--- Frontend/src/components/NoteCard.jsx
+++ Frontend/src/components/NoteCard.jsx
@@ -3,9 +3,25 @@ import { Link } from "react-router";
 import Note from "../../../Backend/src/models/Notes";
 import { PenSquareIcon, Trash2Icon } from "lucide-react";
 import { formatDate } from "../lb/utils";
+import toast from "react-hot-toast";
+import api from "../lb/axios";
 
 const NoteCard = (props) => {
-  const { data } = props;
+  const { data, setNotes } = props;
+
+  const handleDelete = async (e, id) => {
+    e.preventDefault();
+    if (!window.confirm("Are you sure you want to delete this note")) return;
+
+    try {
+      await api.delete(`/notes/${id}`);
+      setNotes((prev)=>prev.filter(data._id != id)) // get rid of deleted 
+      toast.success("note create successfully");
+    } catch (error) {
+      console.log("Error in delete.", error);
+      toast.error("failed to delete the note.");
+    }
+  };
 
   return (
     <Link
@@ -20,12 +36,17 @@ const NoteCard = (props) => {
             {formatDate(new Date(data.createdAt))}
           </span>
           {/* <span>{data.updatedAt}</span> */}
-          <div className="flex items-center gap-1">
-            <PenSquareIcon className="size-4" />
+          <div className="flex items-center justify-center gap-6">
+            <div className="flex items-center gap-1">
+              <PenSquareIcon className="size-4" />
+            </div>
+            <button
+              className="btn btn-xs btn-ghost text-error "
+              onClick={(e) => handleDelete(e, data._id)}
+            >
+              <Trash2Icon className="size-4 text-red-500" />
+            </button>
           </div>
-          <button className="btn btn-xs btn-ghost">
-            <Trash2Icon className="size-4 text-red-500" />
-          </button>
         </div>
       </div>
     </Link>
