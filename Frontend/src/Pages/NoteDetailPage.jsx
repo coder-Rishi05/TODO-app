508cf8d8f9fb09c4973aca4f64922392babe08e9
coder-Rishi05 <rawatrishi1121@gmail.com>
2025-08-09 00:30:43 +0530

    setup the Note detail page

--- Frontend/src/Pages/NoteDetailPage.jsx
+++ Frontend/src/Pages/NoteDetailPage.jsx
@@ -1,6 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router';
+import api from '../lb/axios';
+import toast from 'react-hot-toast';
+
+
+
 
 const NoteDetailPage = () => {
+
+const [note,setNote] = useState(null);
+const [loading,setLoading] = useState(true);
+const [saving,setSaving] = useState(false);
+
+const {id} = useParams()
+
+
+const navigate = useNavigate()
+
+useEffect(()=>{
+
+const fetchNote = async () => {
+  try {
+    const res = await api.get(`/notes/${id}`)
+    setNote(res.data)
+  } catch (error) {
+    console.log("Failed to fetch the note.", error)
+    toast.error("Failed to fetch the note. ")
+  }finally{
+    setLoading(false)
+  }
+}
+
+},[id])
+
+
+console.log(note)
+
+console.log({id});
+
   return (
     <div>NoteDetailPage</div>
   )
