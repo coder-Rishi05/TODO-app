7a78936bc6a55498ebcaf6f32ae04d7d74226d0b
coder-Rishi05 <rawatrishi1121@gmail.com>
2025-08-10 23:25:22 +0530

    added form in Note detail page

--- Frontend/src/Pages/NoteDetailPage.jsx
+++ Frontend/src/Pages/NoteDetailPage.jsx
@@ -44,17 +44,54 @@ const NoteDetailPage = () => {
   }
 
   return (
-    <div className="min-h-screen bg-base-200  ">
+    <div className="min-h-screen  relative h-full w-full ">
+      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_60%,#00FF9D40_100%)]" />
+
       <div className="container mx-auto px-4 py-4">
-        <div className="flex items-center justify-between mb-6  ">
-          <Link to="/" className="btn btn-ghost">
-            <ArrowLeftIcon className="size-5" />
-            Back to notes
-          </Link>
-          <button onClick={handleDelete} className="btn btn-error btn-outline">
-            <Trash2Icon className="size-5" />
-            Delete Note
-          </button>
+        <div className="max-w-2xl mx-auto  ">
+          <div className="flex items-center justify-between mb-6  ">
+            <Link to="/" className="btn btn-ghost">
+              <ArrowLeftIcon className="size-5" />
+              Back to notes
+            </Link>
+            <button
+              onClick={handleDelete}
+              className="btn btn-error btn-outline"
+            >
+              <Trash2Icon className="size-5" />
+              Delete Note
+            </button>
+          </div>
+          <div className="card bg-base-300">
+            <div className="card-body">
+              <div className="form-control mb-4 ">
+                <label className="label">
+                  <span className="label-text">Title</span>
+                </label>
+                <input
+                  className="input pl-6 input-bordered border-green-400 placeholder:text-green-400  "
+                  type="text"
+                  placeholder="Note Title"
+                  value={note.title}
+                  onChange={(e) => setNote({ ...note, title: e.target.value })}
+                />
+              </div>
+              <div className="form-control mb-4 ">
+                <label className="label">
+                  <span className="label-text">Content</span>
+                </label>
+                <textarea
+                  className="textarea  textarea-bordered h-32 mt-4 resize-none border-green-400 placeholder:text-green-400 p-6  "
+                  type="text"
+                  placeholder="Write your notes here..."
+                  value={note.content}
+                  onChange={(e) =>
+                    setNote({ ...note, content: e.target.value })
+                  }
+                />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
