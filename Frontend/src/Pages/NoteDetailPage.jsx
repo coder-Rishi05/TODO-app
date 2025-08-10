4424f0100af106af95902d6ceba30e017ea693df
coder-Rishi05 <rawatrishi1121@gmail.com>
2025-08-10 23:04:39 +0530

    added ui in Notedetail

--- Frontend/src/Pages/NoteDetailPage.jsx
+++ Frontend/src/Pages/NoteDetailPage.jsx
@@ -1,58 +1,64 @@
-import React, { useEffect, useState } from 'react'
-import { useNavigate, useParams } from 'react-router';
-import api from '../lb/axios';
-import toast from 'react-hot-toast';
-import { Loader2Icon } from 'lucide-react';
-
-
-
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router";
+import api from "../lb/axios";
+import toast from "react-hot-toast";
+import { ArrowLeftIcon, Loader2Icon, Trash2Icon } from "lucide-react";
+import { Link } from "react-router";
 
 const NoteDetailPage = () => {
+  const [note, setNote] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [saving, setSaving] = useState(false);
 
-const [note,setNote] = useState(null);
-const [loading,setLoading] = useState(true);
-const [saving,setSaving] = useState(false);
+  const { id } = useParams();
 
-const {id} = useParams()
+  const navigate = useNavigate();
 
+  useEffect(() => {
+    const fetchNote = async () => {
+      try {
+        const res = await api.get(`/notes/${id}`);
+        setNote(res.data);
+      } catch (error) {
+        console.log("Failed to fetch the note.", error);
+        toast.error("Failed to fetch the note. ");
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchNote();
+  }, [id]);
 
-const navigate = useNavigate()
+  console.log(note);
 
-useEffect(()=>{
+  console.log({ id });
 
-const fetchNote = async () => {
-  try {
-    const res = await api.get(`/notes/${id}`)
-    setNote(res.data)
-  } catch (error) {
-    console.log("Failed to fetch the note.", error)
-    toast.error("Failed to fetch the note. ")
-  }finally{
-    setLoading(false)
+  const handleDelete = () => {};
+
+  if (loading) {
+    return (
+      <div className="min-h-screen bg-base-200 flex items-center justify-center">
+        <Loader2Icon className="animate-spin size-10" />
+      </div>
+    );
   }
-}
-fetchNote()
-
-},[id])
-
-
-console.log(note)
-
-console.log({id});
-
-if(loading) {
-  return (
-    <div className='min-h-screen bg-base-200 flex items-center justify-center' >
-      <Loader2Icon className='animate-spin size-10' />
-    </div>
-  )
-}
 
   return (
-    <div>
-      
+    <div className="min-h-screen bg-base-200  ">
+      <div className="container mx-auto px-4 py-4">
+        <div className="flex items-center justify-between mb-6  ">
+          <Link to="/" className="btn btn-ghost">
+            <ArrowLeftIcon className="size-5" />
+            Back to notes
+          </Link>
+          <button onClick={handleDelete} className="btn btn-error btn-outline">
+            <Trash2Icon className="size-5" />
+            Delete Note
+          </button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default NoteDetailPage
+export default NoteDetailPage;
